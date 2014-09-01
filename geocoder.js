f4dc15ad8025d529393d4abe79c01d8df17281d5
Edward Breen <ted.breen@gmail.com>
2014-09-01 16:48:10 -0700

    earthquake finder function

--- geocoder.js
+++ geocoder.js
@@ -2,9 +2,9 @@
 
 var button = document.getElementById('submit');
 
-// button.addEventListener('click', function () {
-//   geocode( encodeLocation() );
-// });
+button.addEventListener('click', function () {
+  geocode( getLoc() );
+});
 
 function getLoc() {
   var city = document.getElementById('city');
@@ -21,12 +21,27 @@ function geocode(loc) {
   var query = {
     'key': 'AIzaSyCIbLULWH2kMQyb2oINrSVmSd4530p6tJA',
     'address': loc
-  }
+  };
   url += $.param(query);  
   $.ajax({
     url: url,
     dataType: 'json',
     method: 'GET',
-    success: function(resp) { console.log(resp); }
+    success: function(resp) {
+      if (resp.status === 'OK') {
+        return resp.results[0];
+      } else {
+        return "that didn't work";
+      }
+    }
   });
+}
+
+function parseResults(result) {
+  var north = result.geometry.bounds.northeast.lat;
+  var east = result.geometry.bounds.northeast.lng;
+  var south = result.geometry.bounds.southwest.lat;
+  var west = result.geometry.bounds.southwest.lng;
+  var lat = result.geometry.location.lat;
+  var lng = result.geometry.location.lng;
 }
