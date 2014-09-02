219c778a182b06ef25366bf5a2fcc7fb603b31ed
Edward Breen <ted.breen@gmail.com>
2014-09-01 22:45:24 -0700

    more cleanup

---
+++ earthquake.js
@@ -0,0 +1,146 @@
+var zoom = 2;
+
+function initializeMap() {
+  var coords = {
+    'lat': 0,
+    'lng': 45
+  };
+  var bounds = {
+    'north': 90,
+    'south': -90,
+    'west': -180,
+    'east': 180,
+    'username': 'tedbreen'
+  };
+  getEarthquakes(coords, bounds);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+      'callback=initializeMap';
+  document.body.appendChild(script);
+}
+
+function recenterMap(coords, quakes) {
+  var mapOptions = {
+    zoom: zoom,
+    center: new google.maps.LatLng(coords.lat, coords.lng)
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  if (quakes.length > 0) {
+    for(var i in quakes) {
+      addMarkers(map, quakes[i]);
+    }
+  }
+}
+
+function addMarkers(map, quake) {
+  var loc = new google.maps.LatLng(quake.lat, quake.lng);
+  var marker = new google.maps.Marker({
+    position: loc,
+    map: map,
+    animation: google.maps.Animation.DROP
+  });
+  var infowindow = infoBox(quake);
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+function dateFormatter(dateStr) {
+  var date = new Date(dateStr);
+  var months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  var month = months[ date.getMonth() ];
+  var num = date.getDate();
+  var year = date.getFullYear();
+  var fullDate = month + " " + num + ", " + year;
+  return fullDate;
+}
+
+function infoBox(quake) {
+  var contentStr = '<p>' + dateFormatter(quake.datetime) + '<p>' +
+    '<p>Magnitude: ' + quake.magnitude + '</p>' +
+    '<p>Depth: ' + quake.depth + '</p>';
+  var infoBox = new google.maps.InfoWindow({
+    content: contentStr
+  });
+  return infoBox;
+}
+
+function submitForm() {
+  var city = document.getElementById('city');
+  var state = document.getElementById('state');
+  var loc = city.value + ', ' + state.value;
+  city.value = "";
+  state.value = "";
+  return loc;
+}
+
+function geocode(loc) {
+  var url = 'https://maps.googleapis.com/maps/api/geocode/json?';
+  var query = {
+    'address': loc
+  };
+  url += $.param(query);
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    method: 'GET',
+    success: function (resp) {
+      var bounds = parseResponse(resp).bounds;
+      console.log(bounds);
+      var coords = parseResponse(resp).coords;
+      getEarthquakes( coords, bounds );
+    }
+  });
+}
+
+function parseResponse(resp) {
+  var bounds = {
+    'north': resp.results[0].geometry.bounds.northeast.lat,
+    'east': resp.results[0].geometry.bounds.northeast.lng,
+    'south': resp.results[0].geometry.bounds.southwest.lat,
+    'west': resp.results[0].geometry.bounds.southwest.lng,
+    'username': 'tedbreen'
+  };
+  var coords = {
+    'lat': resp.results[0].geometry.location.lat,
+    'lng': resp.results[0].geometry.location.lng
+  };
+  var data = {
+    'bounds': bounds,
+    'coords': coords
+  };
+  return data;
+}
+
+function getEarthquakes(coords, bounds) {
+  var url = 'http://api.geonames.org/earthquakesJSON?';
+  url += $.param(bounds);
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    method: 'GET',
+    success: function(resp) {
+      var allQuakes = resp.earthquakes;
+      recenterMap( coords, allQuakes );
+    }
+  });
+}
+
+window.onload = function () {
+ loadScript();
+ var button = document.getElementById('submit');
+ button.addEventListener('click', function () {
+   if (zoom !== 8) {
+     zoom = 8;
+   }
+   geocode( submitForm() );
+ });
+}
+
